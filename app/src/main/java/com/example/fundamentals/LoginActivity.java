f8074da9bc8d8135293fcd075d38e895f54a61a5
Daniel Garcia Ayala <dgayala002@gmail.com>
2022-11-08 14:02:41 +0100

    Primera parte de Glide

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,8 +21,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.loginactivity);
 
         accset=findViewById(R.id.accset);
+        ImageView mGirl = findViewById(R.id.girl);
 
-
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .into(mGirl);
 
     }
 
